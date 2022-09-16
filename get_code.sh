7b1e84e3ee47f7c80b864a588173cc889071ecb8
HERMAN <95212580+Sherman-1@users.noreply.github.com>
2022-09-16 16:28:47 +0200

    Delete get_code.sh

--- get_code.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget -O covid_data.txt https://www.data.gouv.fr/fr/datasets/r/dc7663c7-5da9-4765-%CB%93%E2%86%92a98b-ba4bc9de9079
-awk -F ';' '{if(NR>1){grouped[$2]+=$4}}END{for (key in grouped) { print key "\t" grouped[key] } }' covid_data.txt | sort > covid_aggregated.txt
