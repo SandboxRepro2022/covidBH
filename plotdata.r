7805c57668364ca94d73fce65323ed7db17efb0d
HERMAN Simon <herman.simon.lm@gmail.com>
2022-09-16 17:02:10 +0200

    Deuxieme commit correctif plotdata

--- plotdata.r
+++ plotdata.r
@@ -1,3 +1,3 @@
 coviddata = read.table("covid_aggregated.txt")
-plot(coviddata$V1,coviddata$V2, xlim = max(coviddata$V1), ylim = max(coviddata$V2)
+plot(coviddata$V1,coviddata$V2, xlim = max(coviddata$V1), ylim = max(coviddata$V2))
 lines(coviddata$V1,coviddata$V2)
